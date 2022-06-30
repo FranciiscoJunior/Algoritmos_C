b59c632143d624f587a087673cdce4bc42f2cb1e
Francisco Junior <franciscoamj1001@gmail.com>
2022-06-30 14:31:37 -0300

    .

---
+++ terreno.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    
+    float largura;
+    float comprimento;
+    float metroquadrado;
+    float area;
+    float preco;
+
+    printf("informe a largura do terreno:\n");
+    scanf("%f",&largura);
+
+    printf("informe o comprimento do terreno:\n");
+    scanf("%f",&comprimento);
+
+    printf("informe o valor do metro quadrado:\n");
+    scanf("%f",&preco);
+
+    area = largura * comprimento;
+    preco = area * metroquadrado;
+
+    printf("A  area do terreno e: 8:2%f", area);
+    printf("O preco do terreno e: 8:2%f", preco);
+
+    return 0;
+    system ("PAUSE");
+};
