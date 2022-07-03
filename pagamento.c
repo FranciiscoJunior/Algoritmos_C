9a8a44fc6692b28ff807f1c05189d3db487e7a62
Francisco Junior <franciscoamj1001@gmail.com>
2022-07-02 21:10:59 -0300

    .

---
+++ pagamento.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+
+    char nome[40];
+    float valorHora;
+    float pagamento;
+    int horasTrabalhadas;
+
+    printf("Informe um nome:\n");
+    scanf("%s",&nome);
+
+    printf("Qual o valor pago por horas:\n");
+    scanf("%f",&valorHora);
+
+    printf("Informe a quantidade de horas trabalhadas:\n");
+    scanf("%d",&horasTrabalhadas);
+
+    pagamento = valorHora*horasTrabalhadas;
+
+    printf("O pagamento para o", %s nome, "deve ser", %f pagamento: \n);
+
+return 0;
+system("PAUSE");
+};
