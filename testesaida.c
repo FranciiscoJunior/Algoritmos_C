bfb22aac224d9aad678d17a4210b4753b99c9152
Francisco Junior <franciscoamj1001@gmail.com>
2022-07-08 15:15:40 -0300

    .

---
+++ testesaida.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+
+    char produto, produto1, produto2, genero;
+    float preco1, preco2;
+    int idade, codigo;
+
+    produto1 = computador;
+    produto2 = TV;
+    preco1 = 2100.5;
+    preco2 = 1830.0;
+    idade = 25;
+    codigo = 1925;
+
+    printf("produto: \n");
+    scanf("%s",&produto);
+
+    printf("O produto", %s produto1, "custa RS", %f preco1);
+    printf("O produto", %s produto2, "custa RS", %f preco2);
+    printf("O codigo = :\n",codigo);
+    printf("Dados da pessoa: genero", %s genero, "e idade", %d idade);
+
+return 0;
+system("PAUSE");
+};
